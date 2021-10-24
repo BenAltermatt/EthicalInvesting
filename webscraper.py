b4ee2a50256911d103c4adec42c38268bccdba1f
BenAltermatt <ben.altermatt20@gmail.com>
2021-10-24 09:31:41 -0400

    poopoppepepeeee

--- webscraper.py
+++ webscraper.py
@@ -1,12 +1,67 @@
 # Benjamin Altermatt
 # 2021.10.23
 import requests
+import time as t
+import pickle
 from bs4 import BeautifulSoup as bs
+
+def generateAllCards(tickerFile):
+    # read the file
+    tickerstrings = list()
+    tickers = list()
+    file = open(tickerFile)
+    try:
+        tickerstrings = file.readlines()
+        for x in range(len(tickerstrings)):
+            ticker = tickerstrings[x][:tickerstrings[x].find(',')]
+            if ticker.find('^') == -1:
+                tickers.append(ticker)
+
+    finally:
+        file.close()
+
+    cards = dict()
+
+    og_time = t.time()
+    for tickval in range(len(tickers)):
+        card = generateScorecard(tickers[tickval])
+        print(str((tickval + 1) / len(tickers) * 100) + "% Complete")
+        deltat = t.time() - og_time
+        est_time = (len(tickers) - tickval - 1) * deltat / (tickval + 1)
+        print('Estimated time remaining: ' + str(est_time) + ' seconds')
+        if card is not None:
+            cards[tickval] = card
+
+
+    print(cards)
+
+
+def findTickers():
+    tickers = list()
+
+    # we have to go through every letter in the alphabet
+    for x in range(65, 65 + 26):
+        tickerSite = requests.get("https://www.advfn.com/nyse/newyorkstockexchange.asp?companies=" + chr(x))
+        tickers.extend(scrapeTickers(bs(tickerSite, 'html.parser')))
+    
+    # we have to do the extra characters one
+        tickerSite = requests.get("https://www.advfn.com/nyse/newyorkstockexchange.asp?companies=0")
+        tickers.extend(scrapeTickers(bs(tickerSite, 'html.parser')))
+    
+    return tickers
+
+
+def scrapeTickers(soup):
+    # Unfinished
+    return
+
 
 def generateScorecard(tickVal):
     card = dict()
 
     realTick = getFifthChar(tickVal)
+    if realTick == -1: # data not provided for this ticker
+        return None
 
     page = requests.get('https://www.refinitiv.com/bin/esg/esgsearchresult?ricCode=' + realTick)
 
@@ -19,7 +74,7 @@ def generateScorecard(tickVal):
         score = temp[0]
         toBeParsed = temp[1]
 
-        # associate it with its category
+        # associate it with its category and skip if redundant category
         temp = parseCategory(toBeParsed[toBeParsed.find('TR.'):])
         toBeparsed = temp[1]
 
@@ -57,21 +112,23 @@ def parseCategory(string): # get the category
     return retstr, string[endpoint]
 
 def getFifthChar(ticker):
+    extensions = ['O', 'K']
     finalTicker = ticker
 
     if(len(requests.get('https://www.refinitiv.com/bin/esg/esgsearchresult?ricCode=' + ticker).text) > 2): #no add on
         return ticker
+
+    for x in extensions:
+        if(len(requests.get('https://www.refinitiv.com/bin/esg/esgsearchresult?ricCode=' + ticker + '.' + x).text) > 2): # common O add on
+            return ticker + '.' + x
     
-    if(len(requests.get('https://www.refinitiv.com/bin/esg/esgsearchresult?ricCode=' + ticker + '.O').text) > 2): # common O add on
-        return ticker + '.O'
-
-    for x in range(65, 65 + 26):
-        if(len(requests.get('https://www.refinitiv.com/bin/esg/esgsearchresult?ricCode=' + ticker + '.' + chr(x)).text) > 2): # common O add on
-            return ticker + '.' + chr(x)
-
+    return -1 # no data found for this ticker
 
 def main():
-    print(generateScorecard('GOOGL'))
+    final = generateAllCards('stocks.txt')
+    pickle_out = open("scorecards.pickle","wb")
+    pickle.dump(final, pickle_out)
+    pickle_out.close()
     return
 
 if __name__ == "__main__":
