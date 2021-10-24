f82cd3eed5a22c4b5d295420c8b0e754df7b3db8
BenAltermatt <ben.altermatt20@gmail.com>
2021-10-24 11:35:28 -0400

    Basic outline

--- portfoliobuilder.py
+++ portfoliobuilder.py
@@ -1,12 +1,32 @@
 import pickle
 import sys
 
-pickle_in = open('scorecardsshorter.pickle', 'rb')
-cards = pickle.load(pickle_in)
+cpickle = open('scorecardsshorter.pickle', 'rb')
+cards = pickle.load(cpickle)
+kpickle = open('KPIs.pickle', 'rb')
+kpi = pickle.load(kpickle)
 
-def buildHeap():
-    value = 0
+def makeSuggestions(eth_val):
+    eth_val = eth_val / 100
+    top30 = list()
+    minThreshold = 0
 
+# this is the most basic algorithm for balancing these. linear weighting
+def calculateScore(ticker, eth_val):
+    score = cards[ticker]['Wholistic'] / 100 * eth_val + kpi[ticker] / 10 * (1 - eth_val)
+    if(kpi[ticker] < 0):
+        score = -1000
+    
+    return score
+
+def addToList(stocks, value):
+    for x in range(len(stocks)):
+        if value[1] >= stocks[x][1]:
+
+"""
+def shiftOut(stocks, index):
+    for x in range():
+"""
 
 def main():
     print(cards)
