05cb6d7b798a87874128ceefa020efa9d5f564fd
samichsheperd <shawbyoung@gmail.com>
2021-10-24 03:41:11 -0400

    last commit for the night - parse_thru_tickers has issues with curve fit for some securities

--- assess_value.py
+++ assess_value.py
@@ -8,17 +8,26 @@ from numpy import arange
 import scipy.optimize
 from matplotlib import pyplot
 import requests
-
+from csv import DictReader
+    # iterate over each line as a ordered dictionary
+    
 def parse_thru_tickers():
     out = dict()
-    page = requests.get('https://www.nasdaq.com/market-activity/stocks/screener')
-    print(page)
-    return 1
+    with open('nasdaq_screener.csv', 'r') as read_obj:
+        reader = DictReader(read_obj)
+        for row in reader:
+            try:
+                out[row['Symbol']] = lin_reg_ticker(row['Symbol'])
+            except ValueError:
+                break
+
+    return out
 
 def func(x, a, b, c):
 	return a*np.exp(b*x) + c
     
 def lin_reg_ticker(t):
+    print(t)
     x = list()
     y = list() 
     i = 0
@@ -36,7 +45,7 @@ def lin_reg_ticker(t):
     sigma = np.ones(len(y_arr)) * 0.5 
     p0 = (1, -1, -1)
     popt, _ = scipy.optimize.curve_fit(func,  x_arr,  y_arr, p0, sigma = sigma ,absolute_sigma=True )
-    a, b, c = popt
+    KPI, b, c = popt
    
     # To print a plot of the linear regression  
     #     
@@ -45,28 +54,7 @@ def lin_reg_ticker(t):
     # y_line = func(x_line, a, b, c)
     # pyplot.plot(x_line, y_line, '--', color='red')
     # pyplot.savefig("plot")
-    
-    return {'Ticker' : t, 'KPI' : a}
+    print(KPI)
+    return KPI
 
-p = lin_reg_ticker("TWTR")
 g = parse_thru_tickers()
-
-# list_of_tickers = gt.get_tickers()
-
-
-# msft = yf.Ticker("MSFT")
-# msft_dict = msft.info
-# # print(msft_dict)
-
-# twtr = yf.Ticker("TWTR")
-# twtr_dict =twtr.info
-# print(twtr_dict)
-# # print(list_of_tickers)
-# with open('twitter_info.txt', 'w') as twitter_info:
-#      twitter_info.write(json.dumps(twtr_dict))
-
-# twtr_historical = twtr.history(period="max", interval="1mo")
-# print(twtr_historical["Low"])
-# print(type(twtr_historical).__name__)
-# for index, row in twtr_historical.iterrows():
-#     print(row['Low'])
